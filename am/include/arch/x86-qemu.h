f2c29884894e2aa19ec838edc316938ce8eaa78e
Zebin Wu <zebin.wu@foxmail.com>
2022-06-29 23:44:18 +0800

    Change x86-qemu GPR registers.

--- am/include/arch/x86-qemu.h
+++ am/include/arch/x86-qemu.h
@@ -8,10 +8,10 @@ struct Context {
            eip, cs, eflags, esp, ss3;
 };
 
-#define GPR1 eax
-#define GPR2 ebx
-#define GPR3 ecx
-#define GPR4 edx
+#define GPR1 ebx
+#define GPR2 ecx
+#define GPR3 edx
+#define GPR4 esi
 #define GPRx eax
 
 #endif
